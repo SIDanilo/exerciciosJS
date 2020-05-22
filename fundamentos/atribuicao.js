4684ab7e24fa4e4a4a41357795a4653be26fa871
Danilo Pinheiro <danilopi.cert@gmail.com>
2020-05-22 16:35:38 -0300

    atribuição.js

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,15 @@
+const a = 7
+let b = 3
+
+
+b += a // b = b + a (atribuição aditiva)
+console.log(b)
+
+b -= 4 // b = b - a (atribuição subtrativa)
+console.log(b)
+
+b *= 2 // b = b * 2 (atribuição multiplicativa)
+console.log(b)
+
+b /= 2 // b = b / 2 (atribuição divisiva)
+console.log(b)
