3eeb48031c69f255e783ff5189b6b168a72498a5
Danilo Pinheiro <danilopi.cert@gmail.com>
2020-06-02 17:29:07 -0300

    mNode

--- fundamentos/atribuicao.js
+++ fundamentos/atribuicao.js
@@ -3,7 +3,7 @@ let b = 3
 
 
 b += a // b = b + a (atribuição aditiva)
-console.log(b)
+console.log("" + b)
 
 b -= 4 // b = b - a (atribuição subtrativa)
 console.log(b)
@@ -12,7 +12,4 @@ b *= 2 // b = b * 2 (atribuição multiplicativa)
 console.log(b)
 
 b /= 2 // b = b / 2 (atribuição divisiva)
-console.log(b)
-
-b %=2 // b = b % 2 (resto da divisao)
 console.log(b)
